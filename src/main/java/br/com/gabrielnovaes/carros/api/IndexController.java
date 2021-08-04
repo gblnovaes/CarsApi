cf83d6da99043474fc1052b29a77d9022fb99e2d
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2021-08-04 20:21:30 -0300

    create file structure project

---
+++ src/main/java/br/com/gabrielnovaes/carros/api/IndexController.java
@@ -0,0 +1,16 @@
+package br.com.gabrielnovaes.carros.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping()
+    public String get() {
+        return "API  to Cars";
+    }
+
+
+
+}
--- src/main/java/br/com/gabrielnovaes/carros/domain/CarsRepository.java
+++ src/main/java/br/com/gabrielnovaes/carros/domain/CarsRepository.java
@@ -1,2 +1,6 @@
-package br.com.gabrielnovaes.carros.domain;public interface CarsRepository {
+package br.com.gabrielnovaes.carros.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarsRepository extends CrudRepository<Cars,Long> {
 }
