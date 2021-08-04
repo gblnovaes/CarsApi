48ddb6b3566fa7cfc4ab87b55486bdf6f88e2432
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2021-08-04 20:21:08 -0300

    created cars services

--- src/main/java/br/com/gabrielnovaes/carros/domain/CarsServices.java
+++ src/main/java/br/com/gabrielnovaes/carros/domain/CarsServices.java
@@ -1,2 +1,26 @@
-package br.com.gabrielnovaes.carros.domain;public class CarsServices {
+package br.com.gabrielnovaes.carros.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarsServices {
+
+    @Autowired
+    private CarsRepository repository;
+
+    public List<Cars> getAllCarsFake() {
+        List<Cars> cars = new ArrayList<>();
+        cars.add(new Cars(1L, "Chevette"));
+        cars.add(new Cars(2L, "Honda Civic"));
+        cars.add(new Cars(3L, "Alfa Romeo"));
+        return cars;
+    }
+
+    public Iterable<Cars>  getAllCars() {
+        return repository.findAll();
+    }
 }
